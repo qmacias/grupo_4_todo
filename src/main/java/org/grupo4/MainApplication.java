de04d6063fe4d01cb87b2f35618eb36821a2def8
cquinto <cristian.quinto0125@jala.university>
2025-05-23 14:55:06 -0300

    TSK-1: add task entity

--- src/main/java/org/grupo4/MainApplication.java
+++ src/main/java/org/grupo4/MainApplication.java
@@ -1,8 +1,11 @@
 package org.grupo4;
 
+import org.grupo4.task.domain.Task;
+
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Task task = new Task(1, "Wash the dishes", false);
+        System.out.println(task);
     }
 
 }
